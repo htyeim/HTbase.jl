1d37e1ee0cec0ee29fa994477f19884c0d50b407
htyeim <htyeim@gmail.com>
2020-06-01 23:26:49 +0800

    1

--- src/directory.jl
+++ src/directory.jl
@@ -1,24 +1,4 @@
 
-export add_lockfile
-function add_lockfile(filename::String, sleep_time::Float64 = 16.0)
-    count = 0
-    # sleep_time = 16.9
-    p = dirname(filename)
-    # @show filename, p
-    isdir(p) || mkpath(p)
-    while true
-        if isfile(filename)
-            print(string("\r $count exist! wait $sleep_time s. ", filename, ))
-            sleep(sleep_time)
-            count += 1
-        else
-            # run(`touch $filename`)
-            touch(filename)
-            break
-        end
-    end
-end
-
 
 export check_dir
 function check_dir(dir::String)
@@ -48,11 +28,31 @@ end
 
 
 
+export add_lockfile
+function add_lockfile(filename::String, sleep_time::Float64 = 16.0)
+    count = 0
+    # sleep_time = 16.9
+    p = dirname(filename)
+    # @show filename, p
+    isdir(p) || mkpath(p)
+    while true
+        if isfile(filename)
+            print(string("\r $count exist! wait $sleep_time s. ", filename, ))
+            sleep(sleep_time)
+            count += 1
+        else
+            # run(`touch $filename`)
+            touch(filename)
+            break
+        end
+    end
+end
+
 export rm_lockfile
 function rm_lockfile(filename::String, rm_path = true)
     if isfile(filename)
-        run(`rm $filename`)
-        # rm(filename)
+        # run(`rm $filename`)
+        rm(filename)
     end
     if rm_path 
         rm_dir(dirname(filename))
